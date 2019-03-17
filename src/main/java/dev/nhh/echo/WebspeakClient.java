4b3d891eb8010e4185d37ecb65be835f1f46525d
nhh <Niklas.Hanft@outlook.com>
2019-03-17 22:49:57 +0100

    Set thread names

--- src/main/java/dev/nhh/echo/WebspeakClient.java
+++ src/main/java/dev/nhh/echo/WebspeakClient.java
@@ -6,10 +6,16 @@ import dev.nhh.echo.client.Client;
 public class WebspeakClient {
 
     public static void main(String[] args) {
+
+        // Todo Refactor Microphone into MicrophoneQueue and Thread so the client
+        // can poll and sleep if there is no input
+        
         final var clientThread = new Thread(new Client("localhost"));
+        clientThread.setName("Client Thread");
         clientThread.start();
 
         final var speakerThread = new Thread(new SpeakerThread());
+        speakerThread.setName("Speaker Queue Thread");
         speakerThread.start();
     }
 
