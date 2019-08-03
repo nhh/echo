cdaad1e60aa4dcb54085412c89977dfcf4ecf345
nhh <niklas.hanft@outlook.com>
2019-08-03 23:35:04 +0200

    Revert "Use higher sound settings"
    This reverts commit e0ce6619196e6dfeaab0ba6996fe2d019a41f475.

--- src/main/java/dev/nhh/echo/client/util/EchoAudioFormat.java
+++ src/main/java/dev/nhh/echo/client/util/EchoAudioFormat.java
@@ -3,5 +3,5 @@ package dev.nhh.echo.client.util;
 import javax.sound.sampled.AudioFormat;
 
 public class EchoAudioFormat {
-    public static final AudioFormat EIGHT_BIT_PER_SECOND = new AudioFormat(160000, 8, 1, true, true);
+    public static final AudioFormat EIGHT_BIT_PER_SECOND = new AudioFormat(11025f, 8, 1, true, false);
 }
